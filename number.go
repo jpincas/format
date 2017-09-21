2d1c7163872aea864dac2dbb006d3546832fff4d
jpincas <jon@pincas.co.uk>
2017-09-21 15:50:04 +0200

    take into account negatives in splitHundreths

--- number.go
+++ number.go
@@ -1,6 +1,9 @@
 package format
 
-import "strconv"
+import (
+	"strconv"
+	"strings"
+)
 
 //HundrethsTo2dp takes an integer corresponding to hundreths e.g, cents, and returns a 2dp formatted string
 //Examples:
@@ -8,14 +11,27 @@ import "strconv"
 //65 -> 0.65
 //456 -> 4.65 etc.
 func HundrethsTo2dp(n int) string {
-
+	//If n is exactly 0, just return "0"
+	if n == 0 {
+		return "0"
+	}
+	//Otherwise split
 	left, right := splitHundreths(n)
 	return left + "." + right
 }
 
 func splitHundreths(n int) (left, right string) {
-	//Convert to string and left pad if necessary
+	var sign string
+	//Convert to string
 	s := strconv.Itoa(n)
+	//Left zero padding if necessary
+	//Mechanics depend on whether there is a negative sign or not
+	//If it is a negative number, strip the prefix first and 'cache' it for later
+	if n < 0 {
+		s = strings.TrimPrefix(s, "-")
+		sign = "-"
+	}
+
 	switch len(s) {
 	case 1:
 		s = "00" + s
@@ -23,7 +39,7 @@ func splitHundreths(n int) (left, right string) {
 		s = "0" + s
 	}
 
-	left = s[:len(s)-2]
+	left = sign + s[:len(s)-2]
 	right = s[len(s)-2:]
 
 	return
