d2461b614f6f0ed4ed0698e52a3390faf7dc9053
jpincas <jon@pincas.co.uk>
2018-05-25 16:00:45 +0200

    added rounded currency

--- currency.go
+++ currency.go
@@ -2,18 +2,20 @@ package format
 
 //Currency Symbols
 const (
-	EURSymbol = ""
+	EURSymbol = "€"
 	GBPSymbol = "£"
 )
 
-//EURCentsToCurrency formats as follows: E5.67
 func EURCentsToCurrency(n int) string {
 	return CentsToCurrency(n, EURSymbol, ",", false)
 }
 
-//EURCentsToCurrencyInternational formats as follows: 5,67E
 func EURCentsToCurrencyInternational(n int) string {
 	return CentsToCurrency(n, EURSymbol, ".", true)
+}
+
+func EURCentsToCurrencyInternationalRounded(n int) string {
+	return CentsToCurrencyRounded(n, EURSymbol, ".", true)
 }
 
 //GBPPenceToCurrency formats as follows: £5.67
@@ -24,7 +26,6 @@ func GBPPenceToCurrency(n int) string {
 //CentsToCurrency formats an integer number of cents (or pence etc) to a string
 //with currency symbol and seperator
 func CentsToCurrency(n int, symbol string, separator string, preSymbol bool) string {
-
 	left, right := splitHundreths(n)
 	amount := left + separator + right
 	//Use the flag passed as an argument to determine whether to put the currency
@@ -33,4 +34,15 @@ func CentsToCurrency(n int, symbol string, separator string, preSymbol bool) str
 		return symbol + amount
 	}
 	return amount + symbol
+}
+
+// CentsToCurrencyRounded returns the formated currency amount, without the fractions after the separator
+func CentsToCurrencyRounded(n int, symbol string, separator string, preSymbol bool) string {
+	left, _ := splitHundreths(n)
+	//Use the flag passed as an argument to determine whether to put the currency
+	//symbol at the end of the beginning
+	if preSymbol {
+		return symbol + left
+	}
+	return left + symbol
 }
